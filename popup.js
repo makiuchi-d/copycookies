fd143b36da694dbf28a44c7a61f29ed44929a152
makki_d <d.makiuchi@gmail.com>
2020-10-12 19:26:05 +0900

    impl

---
+++ popup.js
@@ -0,0 +1,17 @@
+chrome.tabs.getSelected(tab => {
+	chrome.cookies.getAll({"url":tab.url}, cookies => {
+		var area = document.createElement('textarea');
+		area.style.position = 'absolute';
+		area.style.border = '0';
+		area.style.padding = '0';
+		area.style.margin = '0';
+		area.style.height = '1px';
+		area.style.top = '-10px';
+		area.innerText = JSON.stringify(cookies);
+		document.body.appendChild(area, document.body.firstChild);
+		area.focus();
+		area.select();
+		document.execCommand("copy");
+		document.body.removeChild(area);
+	});
+});
